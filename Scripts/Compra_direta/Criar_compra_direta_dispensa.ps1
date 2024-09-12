ee91a3e8f881e7342e6b66dba64e197d50dd80b8
Eduardo Pacheco <eduardo.alberto.pacheco34@gmail.com>
2024-09-12 16:58:29 -0300

    Atualizado

---
+++ Scripts/Compra_direta/Criar_compra_direta_dispensa.ps1
@@ -0,0 +1,16 @@
+
+
+$EXECDIR = (Get-Location).Path
+$foldersPath = "${EXECDIR}\test\COMPRA_DIRETA\Dispensa"
+
+if (Test-Path $foldersPath) {
+    $testFiles = Get-ChildItem -Path "$foldersPath\*.robot"
+    Write-Host "Arquivos .robot encontrados em SC_pregao_OPD: $($testFiles | ForEach-Object { $_.FullName })"
+
+    foreach ($testFile in $testFiles) {
+        Write-Host "Executando teste: $($testFile.FullName)"
+        robot --exitonfailure -d ./logs $testFile.FullName
+    }
+} else {
+    Write-Host "O caminho das pastas não existe: $foldersPath"
+}
